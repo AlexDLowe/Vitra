899d85459ae3e8a4118c21b5ab5599f12aa57de4
AlexDLowe <me@alexdonovanlowe.com>
2025-11-06 10:44:53 +0000

    Setting up basice framework for the iOS, watchOS, and package for shared code.

---
+++ Vitra-iOS/Vitra-iOS/Vitra_iOSApp.swift
@@ -0,0 +1,32 @@
+//
+//  Vitra_iOSApp.swift
+//  Vitra-iOS
+//
+//  Created by Alex Donovan-Lowe on 06/11/2025.
+//
+
+import SwiftUI
+import SwiftData
+
+@main
+struct Vitra_iOSApp: App {
+    var sharedModelContainer: ModelContainer = {
+        let schema = Schema([
+            Item.self,
+        ])
+        let modelConfiguration = ModelConfiguration(schema: schema, isStoredInMemoryOnly: false)
+
+        do {
+            return try ModelContainer(for: schema, configurations: [modelConfiguration])
+        } catch {
+            fatalError("Could not create ModelContainer: \(error)")
+        }
+    }()
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+        .modelContainer(sharedModelContainer)
+    }
+}
---
+++ Vitra-watchOS/Vitra-watchOS/Vitra_watchOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  Vitra_watchOSApp.swift
+//  Vitra-watchOS
+//
+//  Created by Alex Donovan-Lowe on 06/11/2025.
+//
+
+import SwiftUI
+
+@main
+struct Vitra_watchOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
